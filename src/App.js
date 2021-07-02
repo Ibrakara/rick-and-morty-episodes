14f7da83951ef6f3d8aadbe4c34bad86980a0519
Ibrakara <ibr.karaduman@gmail.com>
2021-07-02 16:54:49 +0300

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [episodes, setEpisodes] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [episodesPerPage, setEpisodesPerPage] = useState(5)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const episodeResponse = await fetch("https://rickandmortyapi.com/api/episode")
+      const episodesData = await episodeResponse.json()
+      setEpisodes(episodesData)
+    }
+    fetchData()
+    console.log(episodes)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Rick and Morty Episode Guide</h1>
+      
     </div>
   );
 }
